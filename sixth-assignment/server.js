20377d88aafbed3b462b87cffe8c6c9a0fced704
Chandrima Kar <chandrimakar16@gmail.com>
2024-07-05 11:52:52 +0530

    Sixth assignment updated

--- sixth-assignment/server.js
+++ sixth-assignment/server.js
@@ -1,18 +1,35 @@
-const express= require('express')
-const mongoose= require("mongoose")
-const app=express()
-const port = process.env.PORT || 3000;
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const app = express();
+const userRoutes = require('./routes/users');
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.send('Sixth assignment');
+});
 
 
-app.use(express.json())
-app.use(express.urlencoded({extended:false}))
+// URL-encoded password
+const encodedPassword = encodeURIComponent('!@#$%^');
 
-
-
-app.get('/',(req,res)=>{
-    res.send("Sixth assignment")
+// Update connection string
+mongoose.connect(`mongodb+srv://Chandrima:${encodedPassword}@cluster-restapi.hlphbne.mongodb.net/RESAPI(CRUD)?retryWrites=true&w=majority`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
 })
+.then(() => {
+    app.listen(port, () => {
+        console.log(`Server is running on port localhost:${port}`);
+    });
+    console.log('Connected to MongoDB');
+})
+.catch((error) => {
+    console.log(error);
+});
 
-app.listen(port, () => {
-    console.log(`Server is running on port localhost:${port}`);
-  });
+app.use('/users', userRoutes);
